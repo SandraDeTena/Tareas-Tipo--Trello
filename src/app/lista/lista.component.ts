26a7119e892ad75296a9f91a357a8c6a09ae4526
Sandra <dtena90@gmail.com>
2021-05-10 15:43:22 +0200

    Instalar Bootstrap
    1. En la terminal con el  comando de npm install bootstrap
    2. document.angular.json --> en la línea 27 la ruta.

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-lista',
+  selector: 'lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.css']
 })
